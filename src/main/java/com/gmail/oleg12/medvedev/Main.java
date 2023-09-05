2615230137cb0d1c1826d3b59efddd05b282977b
Oleg Medvedev <medvedev.oleg12@gmail.com>
2023-09-06 02:09:06 +0400

    new message added

--- src/main/java/com/gmail/oleg12/medvedev/Main.java
+++ src/main/java/com/gmail/oleg12/medvedev/Main.java
@@ -3,5 +3,6 @@ package com.gmail.oleg12.medvedev;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello qa-guru!");
+        System.out.println("and hello other world!");
     }
 }
